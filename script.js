33995fa813488036f0255783bc355f85199fd235
alexcrown <crowneyalex@gmail.com>
2024-05-13 19:42:09 +0100

    add media queries & fix dark mode

--- script.js
+++ script.js
@@ -1,81 +1,95 @@
+const body = document.body;
 const button = document.querySelector("button");
 const navbar = document.getElementById("navbar");
 const logo = document.getElementById("nav-logo");
-const navLinks = navbar.getElementsByTagName("a");
+const navList = document.getElementById("nav-list").children;
 const aboutMe = document.getElementById("about-me");
+const mePhoto = document.getElementById("me-running");
 const projects = document.getElementById("projects");
 const skills = document.getElementById("skills");
-const body = document.body;
 const footer = document.querySelector("footer");
 const contact = document.getElementById("contact-page");
-let wasExecuted = false;
 
-function nightMode() {
-  if (!wasExecuted) {
-    console.log("Changing to night mode");
-
-    navbar.style.backgroundColor = "var(--night-first-color)";
-    logo.style.filter = "invert(100%)";
-    button.style.backgroundColor = "var(--night-fourth-color)";
-    button.className = "fas fa-sun";
-    button.style.color = "var(--night-text-color)";
-    for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].style.color = "var(--night-text-color)";
-    }
-    aboutMe.style.backgroundColor = "var(--night-second-color)";
-    aboutMe.querySelector("img").style.border = "#9290C3 solid 5px";
-    aboutMe.querySelector("img").style.boxShadow =
-      "6px 6px 3.5px 0px var(--night-text-color)";
-    projects.style.backgroundColor = "var(--night-third-color)";
-    skills.style.backgroundColor = "var(--night-fourth-color)";
-    body.style.color = "var(--night-text-color)";
-    footer.style.color = "var(--night-text-color)";
-    // contact.style.backgroundColor = "var(--night-fifth-color)";
-
-    wasExecuted = true;
-  } else {
-    console.log("Changing back to normal mode");
-
-    navbar.style.backgroundColor = "";
-    logo.style.filter = "";
-    button.style.backgroundColor = "";
-    button.className = "fas fa-moon";
-    button.style.color = "";
-    for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].style.color = "";
-    }
-    aboutMe.style.backgroundColor = "";
-    aboutMe.querySelector("img").style.border = "";
-    aboutMe.querySelector("img").style.boxShadow = "";
-    projects.style.backgroundColor = "";
-    skills.style.backgroundColor = "";
-    body.style.color = "";
-    footer.style.color = "";
-    // contact.style.backgroundColor = "";
-
-    wasExecuted = false;
-  }
-  removeEventListener("click", nightMode);
+function toggleNightMode() {
+  localStorage.setItem(
+    "nightMode",
+    localStorage.getItem("nightMode") === "yes" ? "no" : "yes"
+  );
 }
 
-// function changeToNormalMode() {
-//   if ((navbar.style.backgroundColor = "var(--night-first-color)")) {
-//     navbar.style.backgroundColor = "";
-//     logo.style.filter = "";
-//     button.style.backgroundColor = "";
-//     button.className = "";
-//     button.style.color = "";
-//     for (let i = 0; i < navLinks.length; i++) {
-//       navLinks[i].style.color = "";
-//     }
-//     aboutMe.style.backgroundColor = "";
-//     aboutMe.querySelector("img").style.border = "";
-//     aboutMe.querySelector("img").style.boxShadow = "";
-//     projects.style.backgroundColor = "";
-//     skills.style.backgroundColor = "";
-//     body.style.color = "";
-//     footer.style.color = "";
-//   }
-// }
-
-button.addEventListener("click", nightMode);
+function checkWhichMode() {
+  // NIGHT MODE - HOME
+  if (
+    localStorage.getItem("nightMode") === "yes" &&
+    document.contains(aboutMe)
+  ) {
+    body.classList.add("night-mode");
+    navbar.classList.add("night-mode");
+    logo.classList.add("night-mode");
+    button.classList.remove("fa-moon");
+    button.classList.add("fa-sun");
+    button.classList.add("night-mode");
+    for (let i = 0; i < navList.length; i++) {
+      navList[i].children[0].classList.add("night-mode");
+    }
+    aboutMe.classList.add("night-mode");
+    mePhoto.classList.add("night-mode");
+    projects.classList.add("night-mode");
+    skills.classList.add("night-mode");
+    footer.classList.add("night-mode");
+  }
+  // NIGHT MODE - CONTACT
+  else if (
+    localStorage.getItem("nightMode") === "yes" &&
+    document.contains(contact)
+  ) {
+    body.classList.add("night-mode");
+    navbar.classList.add("night-mode");
+    logo.classList.add("night-mode");
+    button.classList.remove("fa-moon");
+    button.classList.add("fa-sun");
+    button.classList.add("night-mode");
+    for (let i = 0; i < navList.length; i++) {
+      navList[i].children[0].classList.add("night-mode");
+    }
+    contact.classList.add("night-mode");
+    footer.classList.add("night-mode");
+  }
+  // DAY MODE - HOME
+  else if (
+    localStorage.getItem("nightMode") === "no" &&
+    document.contains(aboutMe)
+  ) {
+    body.classList.remove("night-mode");
+    navbar.classList.remove("night-mode");
+    logo.classList.remove("night-mode");
+    button.classList.add("fa-moon");
+    button.classList.remove("fa-sun");
+    button.classList.remove("night-mode");
+    for (let i = 0; i < navList.length; i++) {
+      navList[i].children[0].classList.remove("night-mode");
+    }
+    aboutMe.classList.remove("night-mode");
+    mePhoto.classList.remove("night-mode");
+    projects.classList.remove("night-mode");
+    skills.classList.remove("night-mode");
+    footer.classList.remove("night-mode");
+  }
+  // DAY MODE - CONTACT
+  else if (
+    localStorage.getItem("nightMode") === "no" &&
+    document.contains(contact)
+  ) {
+    body.classList.remove("night-mode");
+    navbar.classList.remove("night-mode");
+    logo.classList.remove("night-mode");
+    button.classList.add("fa-moon");
+    button.classList.remove("fa-sun");
+    button.classList.remove("night-mode");
+    for (let i = 0; i < navList.length; i++) {
+      navList[i].children[0].classList.remove("night-mode");
+    }
+    contact.classList.remove("night-mode");
+    footer.classList.remove("night-mode");
+  }
+}
